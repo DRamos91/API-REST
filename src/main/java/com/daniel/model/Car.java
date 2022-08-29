5d35a3bf0911f6c95a345801513d8ba886d842cc
Daniel <danielmrg@gmail.com>
2022-08-29 18:48:55 -0300

    Getter and Setter with Lombok

--- src/main/java/com/daniel/model/Car.java
+++ src/main/java/com/daniel/model/Car.java
@@ -3,21 +3,29 @@ package com.daniel.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class Car {
 
 	@Id
-	private String _id;
+	@Setter private String _id;
 	
-	private String title;
-	private String brand;
-	private String price;
-	private Integer age;
+	@Getter @ Setter private String title;
+	@Getter @ Setter private String brand;
+	@Getter @ Setter private String price;
+	@Getter @ Setter private Integer age;
+	
+}
 
-	public Car() {
+/**	public Car() {
 		
 	}
 	public Car(String _id, String title, String brand, String price, int age) {
@@ -28,8 +36,9 @@ public class Car {
 		this.price = price;
 		this.age = age;
 	}
+**/
 
-	public String get_id() {
+/**	public String get_id() {
 		return _id;
 	}
 
@@ -68,5 +77,5 @@ public class Car {
 	public void setAge(int age) {
 		this.age = age;
 	}
+**/
 
-}
